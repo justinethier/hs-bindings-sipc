1c8c490f492f1845eea32ed89ed4d64d97a0dfe4
justin <justin.ethier@gmail.com>
2012-02-23 18:30:43 -0500

    Testing for null pointer.

--- src/Examples/MQCreator.hs
+++ src/Examples/MQCreator.hs
@@ -9,7 +9,10 @@ import Bindings.SELinux.SIPC
 main :: IO ()
 main = do
   sipc <- sipc_open "sipc_mq_test" Sipc_creator Sipc_sysv_mqueues 0
-  -- TODO: how to test for NULL pointer?
-  --puStrLn $ show sipc
-  sipc_close(sipc)
-  putStrLn ""
+  -- TODO: need to test below to make sure syntax is OK 
+  case sipc of
+    nullPtr ->
+        putStrLn "Error: Unable to create message queue" -- TODO: send to stderr
+    _ -> do
+        sipc_close(sipc)
+        putStrLn ""
